382bb0d50a0153946f75b2dd9f1a164abe2ac5b2
kimhaengun <chxl89@naver.com>
2021-10-01 17:12:27 +0900

    sql 조인 연습(left,right)

--- sql-practices/sql연습/조인연습.sql
+++ sql-practices/sql연습/조인연습.sql
@@ -28,3 +28,23 @@ select e.first_name, t.title from employees e join titles t on e.emp_no=t.emp_no
 where t.to_date='9999-01-01';
 
 
+-- outer join
+-- insert into dept values(null,'총무');
+-- insert into dept values(null,'영업');
+-- insert into dept values(null,'개발');
+-- insert into dept values(null,'기획');
+select * from dept;
+
+-- insert into emp values(null,'둘리',1);
+-- insert into emp values(null,'마이콜',2);
+-- insert into emp values(null,'또치',3);
+-- insert into emp values(null,'길동',null);
+select * from emp;
+
+select e.name as '이름', d.name as '부서' from emp e join dept d on e.dept_no = d.no;
+
+-- left join
+select e.name as '이름', ifnull(d.name, '없음') as '부서' from emp e left join dept d on e.dept_no = d.no;
+
+-- right join
+select e.name as '이름', d.name as '부서' from emp e right join dept d on e.dept_no = d.no;
