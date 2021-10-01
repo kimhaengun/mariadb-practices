1875a95f8db15dabe726545b4e5cc250bca3b272
kimhaengun <chxl89@naver.com>
2021-10-01 14:18:04 +0900

    select (날짜 뿌수기)

--- sql-practices/sql연습/select02.sql
+++ sql-practices/sql연습/select02.sql
@@ -84,4 +84,35 @@ select concat('---',ltrim('   hello   '),'---'),
        concat('---',rtrim('   hello   '),'---'),
        concat('---',trim(both 'X' from 'xxhelloxxx'),'---');          
        
+-- abs
+select abs(-1), abs(1);
+
+-- mod 
+select mod(10,3);
+
+-- floor
+select floor(3.14);
+
+-- ceil
+select ceil(3.14);
+select ceiling(3.14);
+
+-- round : 반올림
+-- round(x) : x에 가장 근접한 정수
+select round(1.598);
+
+-- round(x,d) : x값 중에 소수점d자리에 가장 근접한 실수
+select round(1.598, 1);
+
+-- pow(x,y) : x의 y승
+select pow(2,10), pow(10,2);
+
+-- sign(x)
+select sign(20), sign(-100), sign(0);
+
+-- greatest(x,y, .......), least(x,y, ........)
+select greatest(10,40,20,30), least(10,40,20,30);
+select greatest('b','A','C'), least('hello','HELLO','HelloH');
+
+-- 함수 : 날짜 함수
 
