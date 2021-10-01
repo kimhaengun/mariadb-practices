b87c7aaa230d9ff574f4742c0068fa1c920b59ce
kimhaengun <chxl89@naver.com>
2021-10-01 14:19:05 +0900

    찐 날짜

--- sql-practices/sql연습/select02.sql
+++ sql-practices/sql연습/select02.sql
@@ -115,4 +115,45 @@ select greatest(10,40,20,30), least(10,40,20,30);
 select greatest('b','A','C'), least('hello','HELLO','HelloH');
 
 -- 함수 : 날짜 함수
+-- CURDATE(), CURRENT_DATE
+select curdate(), current_date();
+
+-- 시간
+-- CURTIME(), CURRENT_TIME
+select curtime(),current_time();
+
+-- now(), sysdate()
+select now();
+select sysdate();
+select now(), sysdate();
+select now(),sleep(2),now();
+select sysdate(),sleep(2),sysdate();
+
+-- date_format(date, format)
+select date_format(now(),'%Y년 %m월 %d일  %h시%i분%s초');
+select date_format(now(),'%Y년 %c월 %d일  %h시%i분%s초');
+
+-- period_diff : 두 날짜의 차이를 알려줌
+-- YYMM, YYYYMM
+-- ex) 근무 개월 수를 출력하시오
+select first_name, period_diff(date_format(curdate(), '%Y%m'), date_format(hire_date,'%Y%m')) as 근속날짜 from employees order by 근속날짜 desc;
+
+-- date_add(=adddate), date_sub(=subdate) 
+-- 날짜를 date에 type(day, month, year) 형식으로 더하거나 뺀다.
+-- ex) 각 사원들의 근무 년수가 5년이 되는 날은 언제인가.
+-- interval 5 year = 5년 / month = 5개월
+select first_name, hire_date, date_add(hire_date, interval 5 month) from employees;
+
+-- cast 데이터 타입 바꾸기 cast('바꿀거' as 타입)
+select '1234'+10, cast('12345' as int) + 10;
+select date_format(cast('2021-10-01' as date), '%Y-%m-%d');
+select cast(1-2 as unsigned);
+select casT(cast(1-2 as unsigned)as signed);
+
+-- mysql, type
+-- 문자 : varchar() < cahr < text < CLOB(Character Large OBject)
+-- 정수 : signed(unsigned), int(integer), medium int, big int, int(11)
+-- float, double
+-- 날짜 : time, date, datetime
+-- LOB : CLOB, BLOB
 
